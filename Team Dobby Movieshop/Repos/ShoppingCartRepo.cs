04a390a9194006753adf3c8b65c9165aad763a55
Erik Risholm <erik.risholm@edu.varberg.se>
2022-10-14 13:44:40 +0200

    Test

--- Team Dobby Movieshop/Repos/ShoppingCartRepo.cs
+++ Team Dobby Movieshop/Repos/ShoppingCartRepo.cs
@@ -52,5 +52,13 @@ namespace Team_Dobby_Movieshop.Repos
             return cartToUpdate;
 
         }
+        public ShoppingCart TestMethod()
+        {
+            ShoppingCart testCart = new ShoppingCart()
+            {
+
+            };
+            return testCart;
+        }
     }
 }
