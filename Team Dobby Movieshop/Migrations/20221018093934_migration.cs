a59bf153a8589363342313d7865de839bb3ff2a1
Erik Risholm <erik.risholm@edu.varberg.se>
2022-10-20 12:03:48 +0200

    aspnet identity

---
+++ Team Dobby Movieshop/Migrations/20221018093934_migration.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Team_Dobby_Movieshop.Migrations
+{
+    public partial class migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_CustomerId",
+                table: "Orders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "Customers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerName",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Customers",
+                table: "Customers",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Customers_CustomerId",
+                table: "Orders",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Customers_CustomerId",
+                table: "Orders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Customers",
+                table: "Customers");
+
+            migrationBuilder.RenameTable(
+                name: "Customers",
+                newName: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_CustomerId",
+                table: "Orders",
+                column: "CustomerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Team Dobby Movieshop/Migrations/20221019151954_AddAdminAccount.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Text;
+
+#nullable disable
+
+namespace Team_Dobby_Movieshop.Migrations
+{
+    public partial class AddAdminAccount : Migration
+    {
+        //USED TO CREATE A USER, ADMIN ROLE AND CONNECT THEM
+
+        const string ADMIN_USER_GUID = "9cbd3cf4-4fc7-11ed-bdc3-0242ac120002";
+        const string ADMIN_ROLE_GUID = "a131739a-4fc7-11ed-bdc3-0242ac120002";
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var hasher = new PasswordHasher<IdentityUser>();
+
+            var passwordHash = hasher.HashPassword(null, "Password100!");
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("INSERT INTO AspNetUsers(Id, UserName, NormalizedUserName, Email, EmailConfirmed, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnabled, AccessFailedCount, NormalizedEmail, PasswordHash, SecurityStamp)");
+            sb.AppendLine("VALUES(");
+            sb.AppendLine($"'{ADMIN_USER_GUID}'");
+            sb.AppendLine(",'admin@svensson.se'");
+            sb.AppendLine(",'ADMIN@SVENSSON.SE'");
+            sb.AppendLine(",'admin@svensson.se'");
+            sb.AppendLine(",0");
+            sb.AppendLine(",0");
+            sb.AppendLine(",0");
+            sb.AppendLine(",0");
+            sb.AppendLine(",0");
+            sb.AppendLine(",'ADMIN@SVENSSON.SE'");
+            sb.AppendLine($",'{passwordHash}'");
+            sb.AppendLine(",''");
+            //sb.AppendLine(",'Admin'");
+            sb.AppendLine(")");
+
+            migrationBuilder.Sql(sb.ToString());
+            migrationBuilder.Sql($"INSERT INTO AspNetRoles (Id, Name, NormalizedName) VALUES ('{ADMIN_ROLE_GUID}','Admin','ADMIN')");
+            migrationBuilder.Sql($"INSERT INTO AspNetUserRoles (UserId, RoleId) VALUES ('{ADMIN_USER_GUID}','{ADMIN_ROLE_GUID}')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"DELETE FROM AspNetUserRoles WHERE UserId ='{ADMIN_USER_GUID}' AND RoleId = '{ADMIN_ROLE_GUID}'");
+            migrationBuilder.Sql($"DELETE FROM AspNetUser WHERE Id ='{ADMIN_USER_GUID}'");
+            migrationBuilder.Sql($"DELETE FROM AspNetRoles WHERE Id ='{ADMIN_ROLE_GUID}'");
+        }
+    }
+}
